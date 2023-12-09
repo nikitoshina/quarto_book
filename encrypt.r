dce6fd24630d32f9a5ee962c61313417ba17fab9
Nikita Tkachenko <nikitoshina@gmail.com>
2023-12-08 19:17:07 -0800

    added password

--- encrypt.r
+++ encrypt.r
@@ -1,2 +1,3 @@
 system('npx staticrypt ./_output/* -r -d ./_output -p APress123! --short --template-color-primary "#1f9e89" --template-color-secondary "#f9f9f3" --template-title "Book Project<br>Nikita Tkachenko" --template-instructions "Enter the Password" --template-button "Access"')
 # system('npx staticrypt ./docs/* -r -d ./docs -p APress123! --short --template-color-primary "#1f9e89" --template-color-secondary "#f9f9f3" --template-title "Report by<br>Bridges & Barriers" --template-instructions "Enter the Password" --template-button "Access"')
+
